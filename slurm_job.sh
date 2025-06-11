507e117f4a74f56c5568e58708cb0464c3070e69
denehoffman <denehoffman@me.com>
2025-06-11 16:12:46 -0400

    ci: fix slurm script to not reinstall library each run

--- slurm_job.sh
+++ slurm_job.sh
@@ -6,13 +6,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=120G
 
-uv venv --python=3.13
-source .venv/bin/activate
-source /raid3/nhoffman/root/root_install_313/bin/thisroot.sh
-
+source /home/nhoffman/.venv/bin/activate
 cd "$PWD" || exit
-uv pip install --reinstall .
-
-which python3
 python3 -m gluex_ksks --chisqdof="$1"
 echo "Done!"
